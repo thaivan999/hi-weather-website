346b2144fab1ce334ade0a9548c60f0f85d2e836
ThaiVanHandSome <danitbadao1234@gmail.com>
2024-11-29 12:27:14 +0700

    init

---
+++ src/collections/Blog.ts
@@ -0,0 +1,23 @@
+import { CollectionConfig } from 'payload'
+
+export const Blog: CollectionConfig = {
+  slug: 'blogs',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+  ],
+}
